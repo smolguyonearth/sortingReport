28e785867e642bfe49f61b347126c8dd320aa86e
smolguyonearth <poynitis@gmail.com>
2024-11-23 18:37:57 +0700

    plotting original to random

--- plotter/plotter_comparison.py
+++ plotter/plotter_comparison.py
@@ -25,7 +25,7 @@ data = [read_log_file(log_file) for log_file in log_files]
 plt.figure(figsize=(12, 8))
 markers = ['o', '+', 'x']  # Different markers for each dataset
 colors = ['b', 'g', 'r']    # Different colors for each dataset
-labels = ['Original', 'Min to Max', 'Max to Min']  # Corresponding labels
+labels = ['Random', 'Min to Max', 'Max to Min']  # Corresponding labels
 
 for i, (sizes, times) in enumerate(data):
     plt.plot(sizes, times, marker=markers[i], color=colors[i], label=labels[i], fillstyle='none')
